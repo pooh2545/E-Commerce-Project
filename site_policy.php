2a82549bdc7fa905e90b2f6b91e52675afb5b3e9
PawarisaJattayaworn <138756381+PawarisaJattayaworn@users.noreply.github.com>
2025-08-23 17:42:58 +0700

    no message

---
+++ site_policy.php
@@ -0,0 +1,64 @@
+<?php
+require_once __DIR__ . '/controller/config.php';
+require_once __DIR__ . '/controller/SiteContentController.php';
+
+$controller = new SiteContentController($pdo);
+$pageData = $controller->getByPageName('เงื่อนไขและนโยบาย');
+?>
+
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($pageData['page_name']) ?></title>
+
+        <link href="assets/css/header.css" rel="stylesheet">
+        <link href="assets/css/footer.css" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f7f7f7;
+    }
+
+        .container {
+            width: 80%;
+            margin: 30px auto;
+            background: #f7f7f7;
+            padding: 30px;
+            border-radius: 10px;
+            line-height: 1.8;
+    }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+            color: #333;
+        }
+    </style>
+</head>
+
+
+<body>
+
+       <!-- Navbar -->
+    <?php include("includes/MainHeader.php"); ?>
+
+<div class="container">
+    <h1><?= htmlspecialchars($pageData['page_name']) ?></h1>
+    <div>
+        <?= $pageData['content'] ?>
+    </div>
+
+    <?php if (!empty($pageData['custom_code'])): ?>
+        <?= $pageData['custom_code'] ?>
+    <?php endif; ?>
+</div>
+
+        <!-- Footer -->
+    <?php include("includes/MainFooter.php"); ?>
+
+</body>
+</html>
