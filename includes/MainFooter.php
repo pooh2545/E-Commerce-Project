1a630358f364f3d04d067109d485aacf3bcf7bb3
PawarisaJattayaworn <138756381+PawarisaJattayaworn@users.noreply.github.com>
2025-09-15 20:46:14 +0700

    แก้Footer

--- includes/MainFooter.php
+++ includes/MainFooter.php
@@ -4,33 +4,34 @@
             <div class="footer-section">
                 <h3>Social Media</h3>
                 <ul>
-                    <li><a>โทร: 082-532- 8622</a></li>
-                    <li><a href="#">อีเมล: shop@example.com</a></li>
-                    <li><a href="#">FB: @myshop</a></li>
-                    <li><a href="#">Line: @myshop</a></li>
+                    <li>โทร: 082-532- 8622</li>
+                    <li>อีเมล: shop@example.com</li>
+                    <li>FB: @myshop</li>
+                    <li>Line: @myshop</li>
                 </ul>
             </div>
             <div class="footer-section">
                 <h3>ความช่วยเหลือ</h3>
                 <ul>
                     <li><a href="#">วิธีการสั่งซื้อ</a></li>
-                    <li><a href="#">คำถามที่พบบ่อย</a></li>
-                    <li><a href="contact_us.php">ติดต่อเรา</a></li>
+                    <li><a href="site_faq.php">คำถามที่พบบ่อย</a></li>
+                    <li><a href="site_contactUs.php">ติดต่อเรา</a></li>
+                    <li><a href="site_aboutUs.php">เกี๋ยวกับเรา</a></li>
                 </ul>
             </div>
             <div class="footer-section">
                 <h3>นโยบายร้าน</h3>
                 <ul>
                     <li><a href="#">นโยบายการคืนสินค้า</a></li>
-                    <li><a href="#">นโยบายความเป็นส่วนตัว</a></li>
+                    <li><a href="site_policy.php">นโยบายความเป็นส่วนตัว</a></li>
                 </ul>
             </div>
             <div class="footer-section">
-                <h3>นโยบายร้าน</h3>
-                <ul>
-                    <li><a href="#">นโยบายการคืนสินค้า</a></li>
-                    <li><a href="#">นโยบายความเป็นส่วนตัว</a></li>
-                </ul>
+                <div class="footer-logo">
+                    <a href="index.php">
+                    <img src="assets/images/Logo.png" alt="Logo">
+                    </a>
+                </div>
             </div>
         </div>
         <div class="footer-bottom">
