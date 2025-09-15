2ed75d2f8c6e895f3a31cd6f0e8ff898901bde21
PawarisaJattayaworn <138756381+PawarisaJattayaworn@users.noreply.github.com>
2025-09-15 16:27:02 +0700

    กำลังแก้ Footer บางส่วน

--- includes/MainFooter.php
+++ includes/MainFooter.php
@@ -4,30 +4,36 @@
             <div class="footer-section">
                 <h3>Social Media</h3>
                 <ul>
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Email: shop@shoestore.com</a></li>
-                    <li><a href="#">Tel: @myshop</a></li>
+                    <li><a>โทร: 082-532- 8622</a></li>
+                    <li><a href="#">อีเมล: shop@example.com</a></li>
+                    <li><a href="#">FB: @myshop</a></li>
                     <li><a href="#">Line: @myshop</a></li>
                 </ul>
             </div>
             <div class="footer-section">
                 <h3>ความช่วยเหลือ</h3>
                 <ul>
-                    <li><a href="#">ค่าจัดส่ง</a></li>
-                    <li><a href="#">ส่งคืนสินค้า</a></li>
+                    <li><a href="#">วิธีการสั่งซื้อ</a></li>
                     <li><a href="#">คำถามที่พบบ่อย</a></li>
                     <li><a href="contact_us.php">ติดต่อเรา</a></li>
                 </ul>
             </div>
             <div class="footer-section">
-                <h3>บริการลูกค้า</h3>
+                <h3>นโยบายร้าน</h3>
                 <ul>
-                    <li><a href="#">บริการหลังการขาย</a></li>
+                    <li><a href="#">นโยบายการคืนสินค้า</a></li>
+                    <li><a href="#">นโยบายความเป็นส่วนตัว</a></li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h3>นโยบายร้าน</h3>
+                <ul>
+                    <li><a href="#">นโยบายการคืนสินค้า</a></li>
                     <li><a href="#">นโยบายความเป็นส่วนตัว</a></li>
                 </ul>
             </div>
         </div>
         <div class="footer-bottom">
-            © 2024 SHOE STORE. All rights reserved
+            © 2025 SHOES JA. All rights reserved
         </div>
     </footer>
